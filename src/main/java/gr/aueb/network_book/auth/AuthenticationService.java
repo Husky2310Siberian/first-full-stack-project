e4280c573a77b091780ed6c505fac8ed61443692
Siberian <vasileios.kritikos@aueb.gr>
2025-02-06 15:07:50 +0200

    update

---
+++ src/main/java/gr/aueb/network_book/auth/AuthenticationService.java
@@ -0,0 +1,13 @@
+package gr.aueb.network_book.auth;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+
+    public void register(RegistrationRequest request) {
+
+
+    }
+}
