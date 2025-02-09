437aa148a3faff37fc94489f1b7506c2b656fada
Siberian <vasileios.kritikos@aueb.gr>
2025-02-09 22:37:31 +0200

    update Book - pagination

--- src/main/java/gr/aueb/network_book/book/BookController.java
+++ src/main/java/gr/aueb/network_book/book/BookController.java
@@ -1,15 +1,13 @@
 package gr.aueb.network_book.book;
 
+import gr.aueb.network_book.common.PageResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("books")
@@ -26,5 +24,21 @@ public class BookController {
     {
         return ResponseEntity.ok(bookService.save(req, connectedUser));
     }
+
+    @GetMapping
+    public ResponseEntity<BookResponse> findBooks(@PathVariable ("book_id")  Integer bookId) {
+
+        return ResponseEntity.ok(bookService.findById(bookId));
+    }
+
+    @GetMapping("{book-id}")
+    public ResponseEntity<PageResponse<BookResponse>> findAllBooks(@RequestParam (name = "page" , defaultValue = "0" , required = false) int page,
+                                                                   @RequestParam (name = "size" , defaultValue = "10", required = false) int size,
+                                                                   Authentication connectedUser)
+            {
+
+        return ResponseEntity.ok(bookService.findAllBooks(page, size ,connectedUser));
+    }
+
 }
 
--- src/main/java/gr/aueb/network_book/book/BookService.java
+++ src/main/java/gr/aueb/network_book/book/BookService.java
@@ -1,9 +1,18 @@
 package gr.aueb.network_book.book;
 
+import gr.aueb.network_book.common.PageResponse;
 import gr.aueb.network_book.user.User;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -20,4 +29,29 @@ public class BookService {
 
         return bookRepository.save(book).getId();
     }
+
+    public BookResponse findById(Integer bookId) {
+        return bookRepository.findById(bookId)
+                .map(bookMapper::toBookResponse)
+                .orElseThrow(() -> new EntityNotFoundException("Book with id: " + bookId + " not found"));
+
+    }
+
+    public PageResponse<BookResponse> findAllBooks(int page, int size, Authentication connectedUser) {
+        User user = ((User) connectedUser.getPrincipal());
+        Pageable pageable = PageRequest.of(page , size , Sort.by("createdDate").descending());
+        Page<Book> books = bookRepository.findAllDisplayedBooks(pageable , user.getId());
+        List<BookResponse> bookResponseList = books.stream().map(bookMapper::toBookResponse).toList();
+        return new PageResponse<>(
+                bookResponseList ,
+                books.getNumber(),
+                books.getSize(),
+                books.getTotalElements(),
+                books.getTotalPages(),
+                books.isFirst(),
+                books.isLast()
+        );
+
+    }
 }
+
